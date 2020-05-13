fc264ebea28a1dd441642e91c7d8ba2886f05070
FiglerReNi <renatafigler85@gmail.com>
2020-05-13 18:43:25 +0200

    forras

--- FirstConnection/src/firstconnection/DB.java
+++ FirstConnection/src/firstconnection/DB.java
@@ -8,17 +8,25 @@ import java.util.logging.Logger;
 
 public class DB {
     /*Library-hez plusz önyvtár adása - jelen esetben szükségünk lesz a driver-re
-      Libraries -> jobb egérgomb -> Add Library -> Java DB Driver
+      Libraries -> jobb egérgomb -> Add Library -> Java DB Driver (kellhet a GlassFish Srever:
+      Tools->Servers)
     */
     final String JDBC_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver"; //ez a derbyhez szükséges driver
-    final String URL = "";
-    final String USERNAME = "";
-    final String PASSWORD = "";
+    //nincsen server a gépen, és sehol ebben a példában, így belső útvonalat adunk meg
+    //sampleDB -> adatbázis neve
+    //create true -> ha nincs még ilyen létrehozza
+    final String URL = "jdbc:derby:sampleDB;create=true";
+//  Belső adatbázisunk lesz, így ez nem feltétlenül kell
+//    final String USERNAME = "";
+//    final String PASSWORD = "";
     
     public DB(){
         try {
-            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+ //           Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("A híd létrejött.");
         } catch (SQLException ex) {
+            System.out.println("Valami baj van.");
             System.out.println("" + ex);
         }
     }
