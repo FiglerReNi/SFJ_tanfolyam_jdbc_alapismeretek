df5f60d9322b1151a5013deee2450a4440f5b2d3
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2021-05-23 20:24:31 +0200

    forras

--- FirstConnection/src/firstconnection/User.java
+++ FirstConnection/src/firstconnection/User.java
@@ -1,12 +1,5 @@
 package firstconnection;
 
-/*POJO - Plain Old Java Object
-    osztályváltozókat, getter/settert és konstruktort tartalmaznak maximum
-    nincs bennük általában semmilyen egyéb függvény/metódus
-    Az adatbázishoz csinált pojo speciális -> entitásnak nevezzük
-    A tábla neve a users, ennek egy sorából csinálunk objektumot a User osztállyal
-        - a tábla minden oszlopához kell osztályváltozó
-        -egy reord (sor) képvisel egy objetumot*/
 public class User {
     private String name;
     private String address;
